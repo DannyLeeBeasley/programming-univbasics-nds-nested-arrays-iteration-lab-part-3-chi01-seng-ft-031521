c6a428631d50cc472a19604dfb1eb5d87074e377
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-06 19:37:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,7 @@ def join_nested_strings(src)
        if src[row_index][element_index].class == String
          strings_only << src[row_index][element_index]
        end
+       strings_only << ''
        element_index += 1
    end
    row_index += 1 
